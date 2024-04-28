9ef69929a32dbdd570b9d953ddf828d58b4931ad
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2024-04-28 06:58:11 +0000

    third

--- modules.js
+++ modules.js
@@ -1,9 +1,11 @@
 const name = require('./names');
 const sayHi = require('./util');
 const data = require('./alternative-syntax');
-console.log(data);
+// console.log(data);
+
+require('./mine-grenade')
 
 
-sayHi('susan')
-sayHi(name.john)
-sayHi(name.peter)
+// sayHi('susan')
+// sayHi(name.john)
+// sayHi(name.peter)
